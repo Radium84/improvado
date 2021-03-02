f847452c844b0c0b5a435ba69cf9afefbabbe41b
Radium84 <73123856+Radium84@users.noreply.github.com>
2021-03-02 17:21:08 +0300

    add comments

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import json
 
 #Создаем массив имен
 Names = ['Ivan','John','Petr','Serg','Helga']
+#Открываем файл для записи
 file = open('Result.csv', 'w')
-file.write('name,age,count'+'\n')
-#Создаем гет запрос
-
+#Создаеем заголовок
+file.write('name,age,count'+'\r\n')
+#Мы будем подставлять имена в get-запрос, с нулевого элемента по конечный элемент
 for i in range(0,len(Names)):
-
+    #Получаем сырой ответ
     Row = requests.get('https://api.agify.io/?name='+Names[i])
+    #Используем библиотеку json для обработки текста из отвеета
     Parsed=json.loads(Row.text)
+    #Используя переменные из json как разделители строки, получаем обработанную строку из значений переменных
     NextStr=Parsed["name"]+',' +str(Parsed["age"])+','+str(Parsed["count"])
-    print(NextStr)
-    file.write(NextStr+'\n')
+    #print(NextStr) проверка результат в консоли
+    #Запись в csv файл,который потом можно будет открыть в Exel
+    file.write(NextStr+'\r\n')
 
 file.close()
